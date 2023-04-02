f09de9195095cd470947af6db55e95bd981b4b7d
Rachel King <G00425694@atu.com>
2023-04-02 10:41:24 +0100

    initial start of research

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pands-project
+
+
+https://www.angela1c.com/projects/iris_project/downloading-iris/
+
+Reference for reading in the file from a URL
---
+++ analysis.py
@@ -0,0 +1,17 @@
+# analysis.py
+# this program outputs a summary of each variable to a single text file
+# and saves a histogram of each variable to png files
+# and outputs a scatter plot of each pair of variables
+
+# Author: Rachel King
+
+import pandas as pd
+
+csv_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+# using the attribute information as the column names
+col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
+iris =  pd.read_csv(csv_url, names = col_names)
+
+print(iris.head())
+print(iris.tail())
+print(iris.dtypes)
